a7c8cd1124f727aea715d086d382ac71be23ff78
Jim Yiu <jyzz2020@gmail.com>
2022-11-19 14:16:09 -0500

    Changed to modified strings

--- test1.py
+++ test1.py
@@ -1,28 +1,35 @@
-# this reverses the words not the letters
-def reverseme2(text):
-    results = list(text.split(' '))
-    results.reverse()  #reverses the words
-    return ' '.join(results)
+import string
 
-# reverses the words
+# this reverses the words not the letters
 def reverseword(text):
-    results = list(text.split(' '))
+    results = list(text.split())
+    results.reverse()  #reverses the words
+    results = ' '.join(results)
+    return results
+
+
+# reverses the letters of the words
+def reverseletter(text):
+    results = list(text.split())
     newword=[]
     for word in results:
         i=len(word)
-        rword=[]
-        for x in range(len(word)):
-            rword += word[i-1]
+        rword=''
+        while i > 0:
+            rword = rword + word[i-1]
             i = i - 1
+        # elimate the spaces
         rword = ' '.join(rword) #change the list to string
+        rword = rword.replace(" ","") #remove whitespace from string
+
         newword.append(rword)
-    print (newword)
+
     newword = ' '.join(newword)
     return newword
 
 
 
-def reverseword2(text):
+def reverseletter2(text):
     words = list(text.split(' ')) #split words in a list
     newlist=[]
     for rev in words:
@@ -38,10 +45,11 @@ def reverseword2(text):
 
 mystring = input("Enter a sentence to input: ")
 print(mystring)
-print(reverseme2(mystring))
 print(reverseword(mystring))
-print(reverseword2(mystring))
-print(rw3(mystring))
+print(reverseletter(mystring))
+print(reverseletter2(mystring))
+
+
 
 
 
