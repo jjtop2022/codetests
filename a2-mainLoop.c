d1971cc2f1f514b3326db2ece2635dbb6ef51445
Jim Yiu <118388922+jjtop2022@users.noreply.github.com>
2022-11-20 14:00:56 -0500

    Update and rename a2-oct30.c to a2-mainLoop.c
    Changed comments slightly.

--- a2-mainLoop.c
+++ a2-mainLoop.c
@@ -143,24 +143,8 @@ int main () {
 
 }
 
-
-
-
-/*******************italiaParshandA2.c***************
-Student Name: Parshand Italia   Email Id: 1192250
-Due Date:  November 4th, 2022   Course Name: Cis 1300
-I have exclusive control over this submission via my password.
-By including this statement in this header comment, I certify that:
-1) I have read and understood the University policy on academic integrity. 2) I
-have completed the Computing with Integrity Tutorial on Moodle; and 3) I have
-achieved at least 80% in the Computing with Integrity Self Test.
-I assert that this work is my own. I have appropriately acknowledged any and
-all material that I have used, whether directly quoted or paraphrased.
-Furthermore, I certify that this assignment was prepared by me specifically for
-this course.
-********************************************************/
-
-/*****  INSTRUCTOR CODE GIVEN FOR THE ASSIGNMENT *****/
+/****************************/
+/*****  FUNCTIONS BELOW *****/
 char getCode (char protein [3]) {
 
     // array allProteins has a list of all 3-triplet amino acids required for this assignment
