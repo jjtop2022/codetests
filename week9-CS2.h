20db49d0c7e9f454a5f1c4e4c267673cb36b82a2
Jim Yiu <118388922+jjtop2022@users.noreply.github.com>
2022-11-20 14:17:09 -0500

    Student base class header file

---
+++ week9-CS2.h
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+class student {
+public:
+	//student() { }
+	student(string nname,int nage,string nmajor,int nID,float ngrade);
+	void addstudent(string nname,int nage,string nmajor,int nID,float ngrade);
+	void printstudent();
+	void changemajor(string nmajor);
+
+	friend void changegrade(student& s, float newgrade);
+	friend void changemajoroutside(student& s, string newmajor);
+private:
+	string name;
+	int age;
+	string major;
+	int ID;
+	float grade;
+
+};
