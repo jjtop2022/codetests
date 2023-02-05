9a025cee7c0fed79c4c77213a9a807a031310702
Jim Yiu <jyzz2020@gmail.com>
2023-02-05 11:54:36 -0500

    update checksystem.py

--- checksystem.py
+++ checksystem.py
@@ -4,7 +4,7 @@ import psutil
 def check_disk_usage(disk):
     du = shutil.disk_usage(disk)
     free = du.free / du.total * 100
-    return free > 18
+    return free > 20
 
 def check_cpu_usage():
     usage = psutil.cpu_percent(1)
