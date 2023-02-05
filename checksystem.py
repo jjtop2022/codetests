f2327745eb1284821b71f268c46a5c3fde2670b3
Jim Yiu <jyzz2020@gmail.com>
2023-02-05 13:24:26 -0500

    update checksystem.py

--- checksystem.py
+++ checksystem.py
@@ -4,14 +4,14 @@ import psutil
 def check_disk_usage(disk):
     du = shutil.disk_usage(disk)
     free = du.free / du.total * 100
-    return free > 20
+    return free
 
 def check_cpu_usage():
     usage = psutil.cpu_percent(1)
     return usage < 75
 
-if not check_disk_usage("/") or not check_cpu_usage():
+if not check_disk_usage("/") > 10 or not check_cpu_usage():
     print("ERROR!")
 else:
-    print("everything is OK")
+    print ("everything is OK {}".format(check_disk_usage("/")))
 
